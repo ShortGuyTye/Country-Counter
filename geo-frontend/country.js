5516c434cd182443aec9aeb94ae936c6891f414d
ShortGuyTye <mtmcrey@yahoo.com>
2026-09-26 00:01:11 -0400

    hamburger fix for countries

--- geo-frontend/country.js
+++ geo-frontend/country.js
@@ -1,21 +1,3 @@
-const params = new URLSearchParams(window.location.search);
-
-const code = params.get("country");
-
-fetch(`/countries/${code}`)
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("country-name").textContent = data.name;
-        document.getElementById("capital").textContent = "Capital City: " + data.capital;
-        document.getElementById("city").textContent = "Largest City: " + data.largest_city;
-        document.getElementById("population").textContent = "Population: " + Number(data.population).toLocaleString();
-        document.getElementById("title").textContent = "CountryInfo | " + data.name;
-        document.getElementById("density").innerHTML = "Density: " + Number(data.density).toLocaleString() + " people per square kilometer";
-        document.getElementById("area").innerHTML = "Land Area: " + Number(data.land_area).toLocaleString() + " km<sup>2</sup>";
-        document.getElementById("birth").textContent = "Birth Rate: " + Number(data.birth_rate).toLocaleString() + " births per 1,000 people";
-        document.getElementById("gdp").textContent = "GDP: $" + Number(data.gdp).toLocaleString();
-    });
-
 // HAMBURGER MENU 
 const hamburger = document.getElementById("hamburger");
 const continentNav = document.getElementById("continent-nav");
@@ -29,3 +11,21 @@ hamburger.addEventListener("click", function () {
         hamburger.setAttribute("aria-label", "Open navigation menu");
     }
 });
+
+const params = new URLSearchParams(window.location.search);
+
+const code = params.get("country");
+
+fetch(`/countries/${code}`)
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("country-name").textContent = data.name;
+        document.getElementById("capital").textContent = data.capital;
+        document.getElementById("city").textContent = data.largest_city;
+        document.getElementById("population").textContent = Number(data.population).toLocaleString();
+        document.getElementById("title").textContent = "CountryInfo | " + data.name;
+        document.getElementById("density").innerHTML = Number(data.density).toLocaleString() + " people per square kilometer";
+        document.getElementById("area").innerHTML = Number(data.land_area).toLocaleString() + " km<sup>2</sup>";
+        document.getElementById("birth").textContent = + Number(data.birth_rate).toLocaleString() + " births per 1,000 people";
+        document.getElementById("gdp").textContent = "$" + Number(data.gdp).toLocaleString();
+    });
